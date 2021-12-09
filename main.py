a345d915ec1bace5200c849cf59e3c4432397944
woniwory <37476534+woniwory@users.noreply.github.com>
2021-12-10 03:33:12 +0900

    graph?

--- main.py
+++ main.py
@@ -23,28 +23,24 @@ df5 = pd.read_csv('./stat_100701.csv', encoding='cp949')
 # #데이터 결측값 확인
 # print(df1.info())
 # print(df1.isnull())
-
-# print(df1.head())
-# print(df1.index)
-# print(df1.values)
 #
 # print(df2.info())
 # print(df2.isnull())
 #
-# # print(df3.info())
-# # print(df3.isnull())
-#
-# # print(df4.info())
-# # print(df4.isnull())
-#
-# # print(df5.info())
-# # print(df5.isnull())
+# print(df3.info())
+# print(df3.isnull())
+
+# print(df4.info())
+# print(df4.isnull())
+
+# print(df5.info())
+# print(df5.isnull())
 
 
 
 #범죄발생이 제일 많은 시간대는 ?
 df1sum1 = df1.sum()
-print(df1sum1)
+df1sum1 = df1sum1.drop('미상')  # 미상값 제외
 
 
 df1sum1.index = df1sum1.index.map(str)
@@ -53,11 +49,11 @@ df1sum1.plot(kind = 'bar',figsize = (20,10), width = 0.6)
 plt.xticks(fontsize=6)
 plt.ylabel("범죄 발생 수")
 plt.xlabel("시간대")
-plt.show()
+#plt.show()
 #21시부터 23시 59분이 가장 범죄 빈도수가 높음을 알 수 있다.
 
 
-# 범죄발생이 제일 많은 시간대는 ?
+# 제일 많이 일어나는 범죄수 ?
 df1sum2 = df1.transpose().sum()
 print(df1sum2)
 
@@ -66,28 +62,9 @@ plt.style.use('ggplot')
 df1sum2.plot(kind = 'bar',figsize = (20,10), width = 0.6)
 plt.xticks(fontsize=6)
 plt.ylabel("범죄 발생 수")
-plt.xlabel("시간대")
-# plt.show()
+plt.xlabel("범죄유형")
+#plt.show()
 #협박, 절도, 교통사고처리특례법 순으로 범죄유형이 제일 많음
-
-#
-# df1 = pd.DataFrame(df1)
-# plt.style.use('ggplot')
-#
-# col_hours = list(map(str,df1.index))
-# df1 = df1.loc[:]
-#
-#
-# fig = plt.figure(figsize=(20, 5))
-# ax = fig.add_subplot(1,1,1)
-#
-# print(col_hours)
-# print(df1.iloc[1,:])
-#
-#
-# ax.plot(col_hours, df1.loc['절도',:], markersize = 5)
-#
-# plt.show()
 
 
 # 1년중 범죄발생이 제일 많은 월은 ?
@@ -99,27 +76,14 @@ plt.style.use('ggplot')
 df2sum1.plot(kind = 'bar',figsize = (20,10), width = 0.6)
 plt.xticks(fontsize=6)
 plt.ylabel("범죄 발생 수")
-plt.xlabel("시간대")
-plt.show()
+plt.xlabel("월대")
+# plt.show()
 #21시부터 23시 59분이 가장 범죄 빈도수가 높음을 알 수 있다.
-
-
-# 범죄발생이 제일 많은 월은 ?
-df2sum2 = df1.transpose().sum()
-print(df2sum2)
-
-df2sum2index = df2sum2.index.map(str)
-plt.style.use('ggplot')
-df2sum2.plot(kind = 'bar',figsize = (20,10), width = 0.6)
-plt.xticks(fontsize=6)
-plt.ylabel("범죄 발생 수")
-plt.xlabel("시간대")
-plt.show()
-#협박, 절도, 교통사고처리특례법 순으로 범죄유형이 제일 많음
 
 
 #범죄발생이 제일 많이 일어나는 장소는 ?
 df3sum1 = df3.sum()
+df3sum1 = df3sum1.drop('기타')  # 미상값 제외
 print(df3sum1)
 
 df3sum1.index = df3sum1.index.map(str)
@@ -127,13 +91,14 @@ plt.style.use('ggplot')
 df3sum1.plot(kind = 'bar',figsize = (20,10), width = 0.6)
 plt.xticks(fontsize=8)
 plt.ylabel("범죄 발생 수")
-plt.xlabel("월")
-plt.show()
+plt.xlabel("장소")
+# plt.show()
 
 
 
 #범죄발생이 제일 많은 지역은 ?
 df4sum1 = df4.sum()
+df4sum1 = df4sum1.drop(['기타 도시', '도시이외'])  # 미상값 제외
 print(df4sum1)
 
 df4sum1.index = df4sum1.index.map(str)
@@ -142,4 +107,4 @@ df4sum1.plot(kind = 'bar',figsize = (20,10), width = 0.6)
 plt.xticks(fontsize=6)
 plt.ylabel("범죄 발생 수")
 plt.xlabel("지역")
-plt.show()
+# plt.show()
